1e5859ef41d7dff9908b32b8e7c027585ab21fec
Aidan Dang <aidandang@gmail.com>
2021-12-24 06:47:49 -0800

    completed reusable code

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,18 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
-const CvsFileReader_1 = require("./CvsFileReader");
-const MatchResult_1 = require("./MatchResult");
-const csvFileReader = new CvsFileReader_1.CsvFileReader('data/football.csv');
-const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+const Summary_1 = require("./Summary");
+const matchReader = MatchReader_1.MatchReader.fromCsv('data/football.csv');
 matchReader.load();
-let manUnitedWinds = 0;
-for (let match of matchReader.matches) {
-    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWinds++;
-    }
-    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWinds++;
-    }
-}
-console.log(`Man United won: ${manUnitedWinds} games.`);
+const summary = Summary_1.Summary.winsAnalysisWithHtmlReport('Man United');
+summary.buildAndPrintReport(matchReader.matches);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,20 +1,9 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CvsFileReader';
-import { MatchResult } from './MatchResult';
+import { Summary } from './Summary';
 
-const csvFileReader = new CsvFileReader('data/football.csv');
-const matchReader = new MatchReader(csvFileReader);
-
+const matchReader = MatchReader.fromCsv('data/football.csv');
 matchReader.load();
 
-let manUnitedWinds = 0;
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
-for (let match of matchReader.matches) {
-	if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-		manUnitedWinds++;
-	} else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-		manUnitedWinds++;
-	}
-}
-
-console.log(`Man United won: ${manUnitedWinds} games.`);
+summary.buildAndPrintReport(matchReader.matches);
